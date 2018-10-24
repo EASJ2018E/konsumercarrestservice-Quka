6e45823097cf1a03e994c7f67eeab8bcabbfb949
Arlind Ukshini <quka93@gmail.com>
2018-10-24 11:26:03 +0200

    update html

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,13 +6,37 @@ interface ICar {
     price: number;
 }
 
-axios.get('http://rest-pele-easj-dk.azurewebsites.net/api/Cars')
-.then(function (response) {
-    let data = console.log(response.data);
-})
-.catch(function (error) {
-    console.log(error);
-});   
 
+function getHttp(): void {
+    
+    let data: ICar[];
+    axios.get <ICar[]> ('http://rest-pele-easj-dk.azurewebsites.net/api/Cars')
+    .then(function (response: AxiosResponse<ICar[]>): void {
+        data = response.data;
+        
+        updateHtml(data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+function updateHtml(data: ICar[]): void {
+    let output = document.getElementById("output");
+
+    data.forEach((car: ICar) => {
+        let res = "<div class='car'>";
+
+            res += "<h2>" + car.vendor + "</h2>";
+            res += "<h3>" + car.model + "</h3>";
+            res += "<span>" + car.price + "</span>";
+
+        res += "</div>";
+        
+        output.innerHTML += res;
+    });
+}
+
+document.getElementById("getHttpBtn").addEventListener("click", getHttp);
 
 // http://rest-pele-easj-dk.azurewebsites.net/api/Cars
